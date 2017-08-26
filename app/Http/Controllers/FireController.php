b58da748cd8e207a9720182e3cdb6a12e6cae7f6
yanun <yanun@loket.com>
2017-08-26 21:46:30 +0700

    transfer

--- app/Http/Controllers/FireController.php
+++ app/Http/Controllers/FireController.php
@@ -10,7 +10,12 @@ class FireController extends Controller
 {
     public function getTopup()
     {
-        return Fire::all();
+        return Fire::where('type_pay', 'topup');
+    }
+
+    public function getTransfer()
+    {
+        return Fire::where('type_pay', 'transfer');
     }
 
     public function topup(Request $request)
@@ -59,13 +64,71 @@ class FireController extends Controller
                     'balance' => ($user->balance + $amount)
                 ]);
             $res['success'] = true;
-            $res['message'] = 'Successfully Created';
+            $res['message'] = 'Successfully Topup';
             $res['data']    = $create;
         }
         else
         {
             $res['success'] = false;
-            $res['message'] = 'Filed to create';
+            $res['message'] = 'Filed to topup';
+        }
+
+        return response()->json($res);
+    }
+
+    public function transfer(Request $request)
+    {
+        $res        = array();
+        $user_id    = $request->input('user_id');
+        $wallet_id  = $request->input('wallet_id');
+        $amount     = (int) $request->input('amount');
+
+        if(empty($wallet_id))
+        {
+            $res['success'] = false;
+            $res['message'] = 'wallet id incorrect!';
+
+            return response()->json($res);
+        }
+
+        if(empty($user_id))
+        {
+            $res['success'] = false;
+            $res['message'] = 'user Id incorrect!';
+
+            return response()->json($res);
+        }
+
+        $user = User::findOrFail($user_id);
+		if(!$user)
+		{
+			$res['success'] = false;
+			$res['message'] = 'Cannot find user!';
+            return response()->json($res);
+		}
+
+        $create = Fire::create([
+            'wallet_id' => $wallet_id,
+            'user_id'   => $user_id,
+            'amount'    => $amount,
+            'type_pay'  => 'transfer',
+            'status'    => 1
+        ]);
+
+        if($create)
+        {
+            User::where('id', $user_id)
+                ->update([
+                    'balance' => ($user->balance - $amount)
+                ]);
+            $res['success'] = true;
+            $res['message'] = 'Successfully Transfer';
+            $res['data']    = $create;
+        }
+        else
+        {
+            $res['success'] = false;
+            $res['message'] = 'Filed to transfer';
         }
 
         return response()->json($res);
